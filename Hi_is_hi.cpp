72b088d96954bd4f2021780acf0ba93d0093e96a
GabeAlbers <ag41930@mail.harpercollege.edu>
2026-08-27 10:29:57 -0500

    Create Hi_is_hi.cpp

---
+++ Hi_is_hi.cpp
@@ -0,0 +1,20 @@
+#include <iostream> 
+
+#include <string> 
+
+using namespace std;
+
+int main()
+{
+
+    char test_letter = 'a';
+
+    string string_test = "test"; //im just tetsing strings out
+
+    'a' < '-'; // this will output a boolian
+
+    int askii_value = static_cast<int>(test_letter); 
+ 
+    cout << "test: " << askii_value;
+
+}
